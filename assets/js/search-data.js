b33e740ee7224d92434be6838cb0f5fcc2a14331
gulefatimakiani <142622879+gulefatimakiani@users.noreply.github.com>
2025-11-24 07:55:44 +0000

    Deploying to gh-pages from @ gulefatimakiani/gulefatimakiani.github.io@0934e1109bb1410bc46ad181cdcd284f21759ddc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,17 +66,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/photo-gallery/";
           
         },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
       },{id: "post-a-post-with-tabs",
         
           title: "a post with tabs",
@@ -297,17 +286,6 @@ ninja.data = [{
             window.location.href = "/blog/2022/giscus-comments/";
           
         },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
       },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
@@ -421,6 +399,18 @@ ninja.data = [{
           section: "News",},{id: "news-presented-our-paper-know-thyself-investigating-social-relevance-and-knowledge-construction-in-engineering-design-research-idetc-2025",
           title: 'Presented our paper! “Know Thyself: Investigating Social Relevance and Knowledge Construction in Engineering...',
           description: "",
+          section: "News",},{id: "news-received-travel-fund-uno-s-student-conference-travel-fund-sctf-sctf-2025",
+          title: 'Received travel fund! UNO’s Student Conference Travel Fund (SCTF) - SCTF, 2025',
+          description: "",
+          section: "News",},{id: "news-awarded-the-uno-graduate-research-and-creative-activity-graca-fund-5-000-to-conduct-the-proposed-study-graca-uno-2025",
+          title: 'Awarded the UNO Graduate Research and Creative Activity (GRACA) fund ($5,000) to conduct...',
+          description: "",
+          section: "News",},{id: "news-our-poster-got-accepted-bridging-the-experience-gap-an-informed-self-placement-approach-to-introductory-programming-sigcse-ts-2025",
+          title: 'Our poster got accepted! “Bridging the Experience Gap: An Informed Self-Placement Approach to...',
+          description: "",
+          section: "News",},{id: "news-officially-a-ph-d-candidate-after-successfully-completing-my-candidacy-exam-2025",
+          title: 'Officially a Ph.D. candidate after successfully completing my candidacy exam - 2025',
+          description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
@@ -471,7 +461,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%6B%69%61%6E%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/gulefatimakiani", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -481,25 +478,25 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/gul-e-fatima-kiani-a2ba33253", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0007-8531-9975", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
